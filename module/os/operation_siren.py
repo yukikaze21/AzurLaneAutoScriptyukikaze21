ee6676bdee8733c541ae37f215d95d1dfa8118c0
ShaddockNH3 <shaddock1122@163.com>
2025-09-03 10:52:59 +0800

    update os1 extra

--- module/os/operation_siren.py
+++ module/os/operation_siren.py
@@ -412,8 +412,9 @@ class OperationSiren(OSMap):
             OpsiGeneral_DoRandomMapEvent=True,
             OpsiGeneral_AkashiShopFilter='ActionPoint',
         )
-        if not self.config.is_task_enabled('OpsiMeowfficerFarming'):
-            self.config.cross_set(keys='OpsiMeowfficerFarming.Scheduler.Enable', value=True)
+        # 手动短猫
+        # if not self.config.is_task_enabled('OpsiMeowfficerFarming'):
+        #     self.config.cross_set(keys='OpsiMeowfficerFarming.Scheduler.Enable', value=True)
         while True:
             # Limited action point preserve of hazard 1 to 200
             self.config.OS_ACTION_POINT_PRESERVE = 200
@@ -428,8 +429,8 @@ class OperationSiren(OSMap):
                 logger.info(f'Reach the limit of yellow coins, preserve={self.config.OS_CL1_YELLOW_COINS_PRESERVE}')
                 with self.config.multi_set():
                     self.config.task_delay(server_update=True)
-                    if not self.is_in_opsi_explore():
-                        self.config.task_call('OpsiMeowfficerFarming')
+                    # if not self.is_in_opsi_explore():
+                    #     self.config.task_call('OpsiMeowfficerFarming')
                 self.config.task_stop()
 
             self.get_current_zone()
