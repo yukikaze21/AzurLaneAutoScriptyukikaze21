3db0346e62b3aba5e47c256e965c81570c6eb2e1
yukikaze21 <hanyecheng0907@gmail.com>
2025-09-30 13:14:51 +0000

    !13 add my fork readme * add my fork readme * 增加侵蚀1暴力移动舰队函数的调用 * update os1 extra

--- module/os/operation_siren.py
+++ module/os/operation_siren.py
@@ -412,8 +412,9 @@ class OperationSiren(OSMap):
             OpsiGeneral_DoRandomMapEvent=True,
             OpsiGeneral_AkashiShopFilter='ActionPoint',
         )
-        if not self.config.is_task_enabled('OpsiMeowfficerFarming'):
-            self.config.cross_set(keys='OpsiMeowfficerFarming.Scheduler.Enable', value=True)
+        # 手动短猫
+        # if not self.config.is_task_enabled('OpsiMeowfficerFarming'):
+        #     self.config.cross_set(keys='OpsiMeowfficerFarming.Scheduler.Enable', value=True)
         while True:
             # Limited action point preserve of hazard 1 to 200
             self.config.OS_ACTION_POINT_PRESERVE = 200
@@ -428,8 +429,8 @@ class OperationSiren(OSMap):
                 logger.info(f'Reach the limit of yellow coins, preserve={self.config.OS_CL1_YELLOW_COINS_PRESERVE}')
                 with self.config.multi_set():
                     self.config.task_delay(server_update=True)
-                    if not self.is_in_opsi_explore():
-                        self.config.task_call('OpsiMeowfficerFarming')
+                    # if not self.is_in_opsi_explore():
+                    #     self.config.task_call('OpsiMeowfficerFarming')
                 self.config.task_stop()
 
             self.get_current_zone()
@@ -456,6 +457,9 @@ class OperationSiren(OSMap):
                 self.globe_goto(self.name_to_zone(zone), types='SAFE', refresh=True)
             self.fleet_set(self.config.OpsiFleet_Fleet)
             self.run_strategic_search()
+
+            # 调用暴力移动函数检查是否漏装置
+            self._execute_fixed_patrol_scan()
 
             self.handle_after_auto_search()
             self.config.check_task_switch()
